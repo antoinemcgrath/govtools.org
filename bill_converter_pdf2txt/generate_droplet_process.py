6f11e9f4e009efa6ebe178b90957d440218dccf2
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-10-22 16:00:34 -0700

    update

--- bill_converter_pdf2txt/generate_droplet_process.py
+++ bill_converter_pdf2txt/generate_droplet_process.py
@@ -10,7 +10,7 @@ import subprocess
 import time
 with open('keys.json') as f:
       credentials = [x.strip().split(',') for x in f.readlines()]
-      the_token = credentials[4]
+      the_token = credentials[3]
       print(the_token)
 
 #### This script will login to Digital Ocean
