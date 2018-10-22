a54b349d28872f161b898e96e85478289db53c3d
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-10-22 12:06:29 -0700

    update

---
+++ bill_converter_pdf2txt/generate_droplet_process.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+## https://cloud.digitalocean.com/droplets?i=9b827d&preserveScrollPosition=false
+## https://github.com/koalalorenzo/python-digitalocean#add-a-tag-to-a-droplet
+## https://github.com/search?q=%22digitalocean.Manager%28token%3D%22&type=Code
+#### pip install -U python-digitalocean
+
+import digitalocean
+import subprocess
+import time
+
+#### This script will login to Digital Ocean
+#### Destroy any existing "process-files" droplet
+#### Create & start a new "process-files" droplet
+#### Determine the new servers IP & SCP a directory to the droplet
+#### SSH into the droplet and execute bash commands
+
+## access from instead /home/crscloud/digitalocean.txt
+## the token =  ...
+new_droplet_name = "process-files"
+manager = digitalocean.Manager(token=the_token)
+keys = manager.get_all_sshkeys()
+
+
+#### Destroying old droplet(s) (if exists)
+def destroy_earlier_droplet(manager, new_droplet_name):
+    my_droplets = manager.get_all_droplets()
+    for droplet in my_droplets:
+        if droplet.name == new_droplet_name:
+            print("Recreating droplet:", droplet.name)
+            droplet.destroy()
+
+
+destroy_earlier_droplet(manager, new_droplet_name)
+
+#### Generating new droplet
+droplet = digitalocean.Droplet(token=the_token,
+                               name=new_droplet_name,
+                               region='nyc2', # New York 2
+                               image='ubuntu-14-04-x64', # Ubuntu 14.04 x64
+                               size_slug='512mb',  # 512MB
+                               tag="disposable",
+                               ssh_keys=keys, #Automatic conversion
+                               backups=False)
+
+
+droplet.create()
+
+
+#### Sleep until droplet build is completed
+time.sleep(8)
+status_is = "starting"
+while status_is != "completed":
+    time.sleep(1.5)
+    actions = droplet.get_actions()
+    for action in actions:
+        action.load()
+        status_is = action.status
+        print (status_is)
+
+
+#### Get droplet IP address
+ip_address = manager.get_droplet(droplet.id).ip_address
+print(ip_address)
+
+
+#### Wait untiil droplet OS is running and then send it files through SCP
+def scp_files(ip_address):
+    print("SCP in 20 secs")
+    time.sleep(20.5)
+    try:
+        subprocess.check_output(["scp",
+        "-r", "-o StrictHostKeyChecking no",
+        "/home/crscloud/congress.ai/public/upload",
+        "root@" + ip_address + ":~/"])
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+    print("SCP done")
+
+
+#### Execute bash commands on droplet through SSH
+def ssh_to_command(ip_address):
+    print("SSH file executing")
+    sshProcess = subprocess.Popen(['ssh',
+                                   "-o StrictHostKeyChecking no",
+                                   "root@" + ip_address],
+                                  stdin=subprocess.PIPE,
+                                  stdout = subprocess.PIPE,
+                                  universal_newlines=True,
+                                  bufsize=0)
+    sshProcess.stdin.write("ls .\n")
+    sshProcess.stdin.write("mkdir WORKED\n")
+    sshProcess.stdin.write("ls .\n")
+    sshProcess.stdin.write("echo END\n")
+    sshProcess.stdin.write("cd ~/uploads\n")
+    sshProcess.stdin.write("ls .\n")
+    #sshProcess.stdin.write("#python3 process.py in.pdf\n")
+    sshProcess.stdin.write("logout\n")
+    sshProcess.stdin.close()
+    for line in sshProcess.stdout:
+        if line == "END\n":
+            break
+        print(line,end="")
+
+    for line in sshProcess.stdout:
+        if line == "END\n":
+            break
+        print(line,end="")
+
+
+scp_files(ip_address)
+ssh_to_command(ip_address)
