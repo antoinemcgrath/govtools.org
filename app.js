f4ccd3bda16c0c9d4606318fea0e63817973fbe0
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-10-26 14:18:58 -0700

    update

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post('/upload', function(req, res) {
 
 	scriptexec = ("python3", ["/home/crscloud/govtools.org/bill_converter_pdf2txt/generate.py", "https://github.com/antoinemcgrath/govtools.org/archive/master.zip"]);
 	//scriptexecute = scriptexec.toString();
-	exec(scriptexecute);
+	exec(scriptexec);
     //
 	///var spawn = require("child_process").spawn;
 	//var process = spawn('python3',["/home/crscloud/govtools.org/public/upload/script.py", '/home/crscloud/govtools.org/public/upload/'+sampleFile.name]);
