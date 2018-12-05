c7d155614a347360740f385dc7bf76990b80c9a6
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-12-05 09:31:14 -0800

    update

---
+++ webapps/bill_converter/run.sh
@@ -0,0 +1,7 @@
+~#!/bin/bash
+#run.sh
+#. run.sh  #.~/run.sh  #sh run.sh  #bash run.sh
+##mkdir output
+##echo "created directory: output"
+##echo "sending " $1  to "~/govtools.org-master/webapps/bill_converter/convertBill2txt.py"
+##python3 ~/govtools.org-master/webapps/bill_converter/convertBill2txt.py $1
